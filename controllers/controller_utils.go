411ebed941520830393467669d8aae578ba77ddf
Yuhui Shi <yuhui@convect.ai>
2021-05-15 23:23:37 -0700

    Controller logic (WIP)

---
+++ controllers/controller_utils.go
@@ -0,0 +1,160 @@
+package controllers
+
+import (
+	"reflect"
+
+	operatorsv2 "convect.ai/notebook-crd/api/v2"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+)
+
+func generateStatefulSet(instance *operatorsv2.Jupyter) *appsv1.StatefulSet {
+	replicas := int32(1)
+
+	statefulSet := &appsv1.StatefulSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      instance.Name,
+			Namespace: instance.Namespace,
+		},
+		Spec: appsv1.StatefulSetSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"statefulset": instance.Name,
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"statefuleset":  instance.Name,
+						"notebook-name": instance.Name,
+					},
+				},
+				Spec: instance.Spec.Template.Spec,
+			},
+		},
+	}
+	// copy all of the Notebook labels to the pod including poddefault related labels
+	labels := &statefulSet.Spec.Template.ObjectMeta.Labels
+	for k, v := range instance.ObjectMeta.Labels {
+		(*labels)[k] = v
+	}
+
+	podSpec := &statefulSet.Spec.Template.Spec
+	container := &podSpec.Containers[0]
+
+	if container.WorkingDir == "" {
+		container.WorkingDir = "/home/jovyan"
+	}
+
+	if container.Ports == nil {
+		container.Ports = []corev1.ContainerPort{
+			{
+				ContainerPort: 8888,
+				Protocol:      "TCP",
+				Name:          "notebook-port",
+			},
+		}
+	}
+
+	return statefulSet
+}
+
+func generateService(instance *operatorsv2.Jupyter) *corev1.Service {
+	port := 8888
+
+	containerPorts := instance.Spec.Template.Spec.Containers[0].Ports
+
+	if containerPorts != nil {
+		port = int(containerPorts[0].ContainerPort)
+	}
+
+	svc := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      instance.Name,
+			Namespace: instance.Namespace,
+		},
+		Spec: corev1.ServiceSpec{
+			Type: "ClusterIP",
+			Selector: map[string]string{
+				"statefulset": instance.Name,
+			},
+			Ports: []corev1.ServicePort{
+				{
+					Name:       "http-" + instance.Name,
+					Port:       80,
+					TargetPort: intstr.FromInt(port),
+					Protocol:   "TCP",
+				},
+			},
+		},
+	}
+
+	return svc
+
+}
+
+// CopyStatefulSetFields copies the owned fields from one StatefulSet to another
+// Returns true if the fields copied from don't match to.
+func copyStatefulSetFields(from, to *appsv1.StatefulSet) bool {
+	requireUpdate := false
+	for k, v := range to.Labels {
+		if from.Labels[k] != v {
+			requireUpdate = true
+		}
+	}
+	to.Labels = from.Labels
+
+	for k, v := range to.Annotations {
+		if from.Annotations[k] != v {
+			requireUpdate = true
+		}
+	}
+	to.Annotations = from.Annotations
+
+	if from.Spec.Replicas != to.Spec.Replicas {
+		to.Spec.Replicas = from.Spec.Replicas
+		requireUpdate = true
+	}
+
+	if !reflect.DeepEqual(to.Spec.Template.Spec, from.Spec.Template.Spec) {
+		requireUpdate = true
+	}
+	to.Spec.Template.Spec = from.Spec.Template.Spec
+
+	return requireUpdate
+}
+
+// CopyServiceFields copies the owned fields from one Service to another
+func copyServiceFields(from, to *corev1.Service) bool {
+	requireUpdate := false
+	for k, v := range to.Labels {
+		if from.Labels[k] != v {
+			requireUpdate = true
+		}
+	}
+	to.Labels = from.Labels
+
+	for k, v := range to.Annotations {
+		if from.Annotations[k] != v {
+			requireUpdate = true
+		}
+	}
+	to.Annotations = from.Annotations
+
+	// Don't copy the entire Spec, because we can't overwrite the clusterIp field
+
+	if !reflect.DeepEqual(to.Spec.Selector, from.Spec.Selector) {
+		requireUpdate = true
+	}
+	to.Spec.Selector = from.Spec.Selector
+
+	if !reflect.DeepEqual(to.Spec.Ports, from.Spec.Ports) {
+		requireUpdate = true
+	}
+	to.Spec.Ports = from.Spec.Ports
+
+	return requireUpdate
+}
--- controllers/jupyter_controller.go
+++ controllers/jupyter_controller.go
@@ -20,7 +20,12 @@ import (
 	"context"
 
 	"github.com/go-logr/logr"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	apierrs "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -30,13 +35,17 @@ import (
 // JupyterReconciler reconciles a Jupyter object
 type JupyterReconciler struct {
 	client.Client
-	Log    logr.Logger
-	Scheme *runtime.Scheme
+	Log      logr.Logger
+	Scheme   *runtime.Scheme
+	Recorder record.EventRecorder
 }
 
 //+kubebuilder:rbac:groups=operators.convect.ai,resources=jupyters,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=operators.convect.ai,resources=jupyters/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=operators.convect.ai,resources=jupyters/finalizers,verbs=update
+// +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
+// +kubebuilder:rbac:groups=core,resources=services,verbs="*"
+// +kubebuilder:rbac:groups=apps,resources=statefulsets,verbs="*"
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -48,9 +57,81 @@ type JupyterReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.7.2/pkg/reconcile
 func (r *JupyterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = r.Log.WithValues("jupyter", req.NamespacedName)
+	log := r.Log.WithValues("jupyter", req.NamespacedName)
 
-	// your logic here
+	instance := &operatorsv2.Jupyter{}
+	if err := r.Get(ctx, req.NamespacedName, instance); err != nil {
+		log.Error(err, "unable to fetch notebook")
+		if apierrs.IsNotFound(err) {
+			return ctrl.Result{}, nil // Ignore not found error
+		}
+		return ctrl.Result{}, err
+
+	}
+
+	// Reconcile statefulset
+	ss := generateStatefulSet(instance)
+
+	if err := ctrl.SetControllerReference(instance, ss, r.Scheme); err != nil {
+		return ctrl.Result{}, err
+	}
+
+	// Check if the statefulset already exists
+	foundStateful := &appsv1.StatefulSet{}
+	justCreate := false
+	err := r.Get(ctx, types.NamespacedName{Name: ss.Name, Namespace: ss.Namespace}, foundStateful)
+
+	if err != nil && apierrs.IsNotFound(err) {
+		// Not found, create new
+		log.Info("Creating StatefulSet", "namespace", ss.Namespace, "name", ss.Name)
+		if err = r.Create(ctx, ss); err != nil {
+			log.Error(err, "unable to create StatefulSet")
+			return ctrl.Result{}, err
+		}
+		justCreate = true
+
+	} else if err != nil {
+		log.Error(err, "error getting StatefulSet")
+		return ctrl.Result{}, err
+	}
+	// Update the foundStateful object and write the result back if there are any changes
+	if !justCreate && copyStatefulSetFields(ss, foundStateful) {
+		log.Info("Updating StatefulSet", "namespace", ss.Namespace, "name", ss.Name)
+		if err = r.Update(ctx, foundStateful); err != nil {
+			log.Error(err, "unable to update StatefulSet")
+			return ctrl.Result{}, err
+		}
+	}
+
+	// Reconcile service
+	svc := generateService(instance)
+
+	foundService := &corev1.Service{}
+	justCreate = false
+	err = r.Get(ctx, types.NamespacedName{Name: svc.Name, Namespace: svc.Namespace}, foundService)
+
+	if err != nil && apierrs.IsNotFound(err) {
+		log.Info("Creating service", "namespace", svc.Namespace, "name", svc.Name)
+		if err = r.Create(ctx, svc); err != nil {
+			log.Error(err, "unable to create service")
+			return ctrl.Result{}, err
+		}
+		justCreate = true
+	} else if err != nil {
+		log.Error(err, "error getting service")
+		return ctrl.Result{}, err
+	}
+
+	// Update the service object if needed
+	if !justCreate && copyServiceFields(svc, foundService) {
+		log.Info("Updating service", "namespace", svc.Namespace, "name", svc.Name)
+		if err = r.Update(ctx, foundService); err != nil {
+			log.Error(err, "unable to update service")
+			return ctrl.Result{}, err
+		}
+	}
+
+	// Update the status
 
 	return ctrl.Result{}, nil
 }
